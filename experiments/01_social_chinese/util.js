c0f04893c8beb513eaeca890a3e82e5ddfdcc18b
irenegreenbean <ireneyi@berkeley.edu>
2023-05-15 07:13:53 -0700

    adding more trials etc

--- experiments/01_social_chinese/util.js
+++ experiments/01_social_chinese/util.js
@@ -10,17 +10,31 @@ function evaluate_response(data) {
         }
     }
 
+// function create_tv_array(json_object) {
+//     let tv_array = [];
+//     for (let i = 0; i < json_object.length; i++) {
+//         obj = {};
+//         obj.stimulus = json_object[i].stimulus;
+//         obj.data = {};
+//         obj.data.correct = json_object[i].correct;
+//         tv_array.push(obj)
+//     }
+//     return tv_array;
+// }
+
 function create_tv_array(json_object) {
     let tv_array = [];
     for (let i = 0; i < json_object.length; i++) {
         obj = {};
         obj.stimulus = json_object[i].stimulus;
         obj.data = {};
-        obj.data.correct = json_object[i].correct;
+        obj.data.clip = json_object[i].clip;
+        obj.data.speaker = json_object[i].speaker;
         tv_array.push(obj)
     }
     return tv_array;
-}
+};
+
 
 function shuffle_array(arr) {
     for (let i = arr.length - 1; i > 0; i--) {
